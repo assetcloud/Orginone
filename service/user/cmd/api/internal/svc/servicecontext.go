d3f8810a02b91b2180e5fb2775a42d2305c7e1d8
panzhaohui <panzhaohui@msn.com>
2021-03-02 22:38:09 +0800

    model依赖

--- service/user/cmd/api/internal/svc/servicecontext.go
+++ service/user/cmd/api/internal/svc/servicecontext.go
@@ -2,14 +2,22 @@ package svc
 
 import (
 	"user/cmd/api/internal/config"
+	“user/model”
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config config.ConfigUser
+	Model model.UserModel
+
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	return &ServiceContext{
-		Config: c,
-	}
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
+    um := model.NewUserModel(conn)
+    // redis cache 模式
+    // um := NewUserModel(conn,c.CacheRedis)
+    return &ServiceContext{
+        Config:    c,
+        UserModel: um,
+    }
 }
